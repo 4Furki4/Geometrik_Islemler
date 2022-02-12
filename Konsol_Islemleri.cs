76a1186e4e89a416eb807c3e8b1eea36fde42e02
Furkan Cengiz <muhammedcengiz1@gmail.com>
2022-02-12 22:16:04 +0300

    UzunlukMesajı adlı, uzunluk almadan önce rica mesajı ve talimat mesajı yazdıran metot ile KenarUzunlukları adlı, string alınan kenar uzunluklarını int yapıp bunu int dizide depolayan metot oluşturuldu.

--- Konsol_Islemleri.cs
+++ Konsol_Islemleri.cs
@@ -2,7 +2,7 @@ using System;
 namespace Geometrik_Islemler
 {
     public class Konsol_Islemleri
-    {
+    {   string Sekil="";
         // Geometrik şekil - kenar bilgileri - hesaplanacak işlem 
         public void IlkMesaj()
         {
@@ -12,29 +12,50 @@ namespace Geometrik_Islemler
         public string SekilBelirleme()
         {
             int input = int.Parse(Console.ReadLine());
-            string Sekil="";
-            switch (input)
-            {
-                case 1:
-                    Sekil="Kare";
-                    break;
-                case 2:
-                    Sekil="Dikdörtgen";
-                    break;
-                case 3:
-                    Sekil="Üçgen";
-                    break;
-                case 4:
-                    Sekil="Daire";
-                    break;
-                case 5:
-                    Sekil="Küp";
-                    break;
-                case 6:
-                    Sekil="Silindir";
-                    break;
-            }
+            Sekil="";
+                switch (input)
+                {
+                    case 1:
+                        Sekil="Kare";
+                        break;
+                    case 2:
+                        Sekil="Dikdörtgen";
+                        break;
+                    case 3:
+                        Sekil="Üçgen";
+                        break;
+                    case 4:
+                        Sekil="Daire";
+                        break;
+                    case 5:
+                        Sekil="Küp";
+                        break;
+                    case 6:
+                        Sekil="Silindir";
+                        break;
+                }
             return Sekil;
         }
+        public void UzunlukMesajı() // Uzunlukları isteyen mesajı, gerekli talimatları yazdıran metot.
+        {
+            System.Console.WriteLine("Lütfen talimatlara uygun bir şekilde girmek istediğiniz kenar uzunluklarını yazınız:");
+            System.Console.WriteLine("Her uzunluğun arasına boşluk bırakınız!");
+            System.Console.WriteLine("Kare ve küp için tek kenarın, dikdörtgen için 2 kenarın, üçgen için üç kenarın, daire için yarı çapının, silindir için  yarı çapın ve yüksekliğin uzunluğunu yazınız.");
+        }
+        public int[] KenarUzunlukları() //kenar uzunluklarını string olarak aldık ve bunları int değere dönüştürüp int dizide depoladık.
+        {
+            
+            string s= Console.ReadLine();
+            string[] arr =s.Split(" ");
+            int[] uzunluklar= new int[4];
+            int i=0;
+                foreach (var item in arr)           
+                {
+                    uzunluklar[i]=int.Parse(item);
+                    i++;
+                }
+            return uzunluklar;
+        }
+        
     }
 }
