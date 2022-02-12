92315858d731ebc1944d12b4deb62c477dbd2c9a
Furkan Cengiz <muhammedcengiz1@gmail.com>
2022-02-12 22:55:43 +0300

    Gemoretrik_Islemler sınıfında CevreHesaplama ve AlanHesaplama metotları oluşturuldu

--- Geometrik_Islemler.cs
+++ Geometrik_Islemler.cs
@@ -2,6 +2,27 @@ namespace Geometrik_Islemler
 {
     public class Geometrik_Islemler
     {
-        
+        Konsol_Islemleri konsol= new Konsol_Islemleri();
+        public int CevreHesaplama(int[] arr)
+        {
+            int cevre=0;
+            foreach (var item in arr)
+            {
+                cevre+=item;
+            }
+            return cevre;
+        }
+        public int AlanHesaplama(int[] arr)
+        {
+            int alan =1;
+            if (konsol.SekilBelirleme()!="Silindir" && konsol.SekilBelirleme()!= "Daire")
+            {
+                foreach (var item in arr)
+                {
+                    alan*=item;
+                }
+            }
+            return alan;
+        }
     }
 }
