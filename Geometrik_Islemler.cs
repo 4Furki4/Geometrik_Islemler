f5312bba3d6568b244d810fc02173389dfd2a2f9
Furkan Cengiz <muhammedcengiz1@gmail.com>
2022-02-12 23:14:44 +0300

    CevreHesaplama metodunda silindir ve dairenin çevresini hesaplamak için if-else yapısı kullanılarak yeni işlemler eklendi.

--- Geometrik_Islemler.cs
+++ Geometrik_Islemler.cs
@@ -1,15 +1,27 @@
+using System;
 namespace Geometrik_Islemler
 {
     public class Geometrik_Islemler
     {
         Konsol_Islemleri konsol= new Konsol_Islemleri();
-        public int CevreHesaplama(int[] arr)
+        public double CevreHesaplama(int[] arr)
         {
-            int cevre=0;
-            foreach (var item in arr)
+            double cevre=0;
+            if (konsol.SekilBelirleme()!="Silindir" && konsol.SekilBelirleme()!= "Daire")
             {
-                cevre+=item;
+                foreach (var item in arr)
+                {
+                    cevre+=item;
+                }
+            }else if (konsol.SekilBelirleme()=="Silindir")
+            {
+                cevre= ((Math.PI*arr[0])*2)+(((Math.PI*arr[0])*2)+arr[1])*2;
             }
+            else if (konsol.SekilBelirleme()=="Daire")
+            {
+                cevre=((Math.PI*arr[0])*2);
+            }
+            
             return cevre;
         }
         public int AlanHesaplama(int[] arr)
