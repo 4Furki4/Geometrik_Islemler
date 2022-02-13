6fb48d82275d140319ed7f607ea3add0542c165d
Furkan Cengiz <muhammedcengiz1@gmail.com>
2022-02-13 17:28:58 +0300

    Metotlara konsoldan alınan sekli parametre olarak ekledik ve islemleri bu parametreye göre çalıştıracak.

--- Geometrik_Islemler.cs
+++ Geometrik_Islemler.cs
@@ -4,47 +4,63 @@ namespace Geometrik_Islemler
     public class Geometrik_Islemler
     {
         Konsol_Islemleri konsol= new Konsol_Islemleri();
-        public double CevreHesaplama(int[] arr)
+        public double CevreHesaplama(int[] arr, string sekil)
         {
             double cevre=0;
-            if (konsol.SekilBelirleme()!="Silindir" && konsol.SekilBelirleme()!= "Daire")
+            if (sekil!="Silindir" && sekil!= "Daire")
             {
                 foreach (var item in arr)
                 {
                     cevre+=item;
                 }
-            }else if (konsol.SekilBelirleme()=="Silindir")
+            }else if (sekil=="Silindir")
             {
                 cevre= ((Math.PI*arr[0])*2)+(((Math.PI*arr[0])*2)+arr[1])*2;
             }
-            else if (konsol.SekilBelirleme()=="Daire")
+            else if (sekil=="Daire")
             {
                 cevre=((Math.PI*arr[0])*2);
             }
             
             return cevre;
         }
-        public double AlanHesaplama(int[] arr)
+        public double AlanHesaplama(int[] arr,string sekil)
         {
             double alan =1;
-            if (konsol.SekilBelirleme()!="Silindir" && konsol.SekilBelirleme()!= "Daire" && konsol.SekilBelirleme()!= "Üçgen")
+            if (sekil!="Silindir" && sekil!= "Daire" && sekil!= "Üçgen")
             {
                 foreach (var item in arr)
                 {
                     alan*=item;
                 }
-            }else if (konsol.SekilBelirleme()=="Silindir") // Alan formülü 2 π.r2+ 2πrh
+            }else if (sekil=="Silindir") // Alan formülü 2 π.r2+ 2πrh
             {
                 alan = (2*(Math.PI*Math.Pow(arr[0],2))+ 2*((Math.PI*arr[0]*arr[1])));
-            }else if (konsol.SekilBelirleme()== "Daire")
+            }else if (sekil== "Daire")
             {
                 alan = Math.PI*Math.Pow(arr[0],2);
             }
-            else if (konsol.SekilBelirleme()== "Üçgen")
+            else if (sekil== "Üçgen")
             {
                 alan = (arr[2]*arr[3])/2;
             }
             return alan;
+        }
+
+        public double HacimHesaplama(int[] arr, string sekil)
+        {
+            
+            double hacim=1;
+            switch (sekil)
+            {
+                case "Silindir":
+                hacim=Math.PI*Math.Pow(arr[0],2)*arr[1]; 
+                break;
+                case "Küp":
+                hacim=Math.Pow(arr[0],3);
+                break;
+            }
+            return hacim; 
         }
     }
 }
