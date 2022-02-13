3c9e187e62d33cee2ec3f65b6ca0f35978b7e6c6
Furkan Cengiz <muhammedcengiz1@gmail.com>
2022-02-13 17:46:50 +0300

    Main metoduna sınıflardan nesne oluşturulup metotlara ulaşarak ilk deneme yapılıyor.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,24 @@ namespace Geometrik_Islemler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Konsol_Islemleri konsol = new Konsol_Islemleri();
+            Geometrik_Islemler Islemler= new Geometrik_Islemler();
+            konsol.IlkMesaj();
+            string sekil= konsol.SekilBelirleme();
+            string Islem= konsol.SecilenIslem();
+            int[] Uzunluklar = konsol.KenarUzunlukları();
+            switch (Islem)
+            {
+                case "Çevre":
+                konsol.SonucuYazdırma(sekil,Islem,Islemler.CevreHesaplama(Uzunluklar,sekil));
+                break;
+                case "Alan":
+                konsol.SonucuYazdırma(sekil,Islem,Islemler.AlanHesaplama(Uzunluklar,sekil));
+                break;
+                case "Hacim":
+                konsol.SonucuYazdırma(sekil,Islem,Islemler.HacimHesaplama(Uzunluklar,sekil));
+                break;
+            }            
         }
     }
 }
